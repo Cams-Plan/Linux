635cec58fbcd3e7f2aca81e49c5d868245579362
Camille Francis <110306796+Cams-Plan@users.noreply.github.com>
2022-10-30 17:30:50 +0000

    changes to the info file

--- Hide n Seek Bash Script/.hide_seek.sh
+++ Hide n Seek Bash Script/.hide_seek.sh
@@ -1,35 +1,33 @@
 #!/usr/bin/bash
-
 printf "\nYou've found me!\n"
+sleep 1
 printf "\nGood job...\n"
+
 # wait for 3 seconds - contemplate coming out
 sleep 2
 printf "\nFine,"
 sleep 1
-printf " I will come out now.\n"
+printf " I'll come out now.\n"
 sleep 1
 printf "\n      UN-HIDING FILE...\n"
-# *UN-HIDE THE FILE*
-# use the line of commands directly below to allow execution from any directory:
-#mv [/full/path/].hide_seek.sh [/full/path/]hide_seek.sh
 
-# use this command if bash is executed within file's directory (this version is restrictive):
-mv .hide_seek.sh hide_seek.sh
+# Un-hide the file
+# *EDIT THE COMMAND BELOW* - insert your home directory path into the chevrons (<>) - using the path allows execution from any dir:
+mv /<HOME DIRECTORY>/test_scripts/.hide_seek.sh /<HOME DIRECTORY>/test_scripts/hide_seek.sh
 sleep 2
 printf "\n\nI'm out of hiding now. See?\n\n"
 sleep 1
 
-# *RECURSIVELY LIST PARENT DIRECTORY OF FILE*
-# use this line of commands to allow execution from any directory:
-#ls -R [/full/path/]
-
-# use this command if bash is executed within file's directory (this version is restrictive):
-ls -R
+# *EDIT THE COMMAND BELOW* - list the files in the directory where the .hide_seek file resides on your system
+# using an option to list the files per line
+ls -1 /<HOME DIRECTORY>/test_scripts
 sleep 4
 printf "\n\nSee, no '.' before the file name (hide_seak.sh).\n"
 sleep 1
-printf "\n      FINDING FILE...\n\n"
+printf "\n      FINDING FILE..."
 sleep 2
+printf "\n      file found.\n\n"
+sleep 1
 
 # find the newly un-hidden/visible file
 find -iname hide_seek.sh
@@ -40,12 +38,9 @@ printf "        See you later!\n"
 sleep 1
 printf "\n      HIDING FILE..."
 
-# *HIDE THE FILE*
-# use the line of commands directly below to allow execution from any directory:
-#mv [/full/path/]hide_seek.sh [/full/path/].hide_seek.sh
-
-# use this command if bash is executed within file's directory (this version is restrictive):
-mv hide_seek.sh .hide_seek.sh
+# Hide the file
+# *EDIT THE COMMAND BELOW* - insert your home directory path in place of the chevrons (<>) - using the path allows execution from any dir:
+mv /<HOME DIRECTORY>/test_scripts/hide_seek.sh /<HOME DIRECTORY>/test_scripts/.hide_seek.sh
 sleep 2
 printf "\n      File hidden successfully.\n"
 
